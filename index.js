66c1a014be371e339bfd9926210102654f924431
EugenioProkopenko <ievgeniiprokopenko@gmail.com>
2026-03-24 12:30:38 +0000

    Deploying to gh-pages from @ EugenioProkopenko/goit-js-hw-12@fce7674d546a90b23e98c9e7c804978f9cd010c8 🚀

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as l}from"./assets/vendor-MoVzAjbq.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function a(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(e){if(e.ep)return;e.ep=!0;const s=a(e);fetch(e.href,s)}})();const c="49627476-e05c06d50af5be4d22ab8156c";async function f(t,r){const e="https://pixabay.com/api/",s={key:c,q:t,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15,page:r};return(await l.get(e,{params:s})).data}document.querySelector(".gallery");function u(t){const r=t.tags.split(",").slice(0,3).join(", ");return`
+    <li class="gallery-item">
+        <a href="${t.largeImageURL}" class="gallery-link">
+            <img
+                class="gallery-image"
+                src="${t.webformatURL}"
+                alt="${r}"
+            />
+            <div class="info">
+                <div class="info-item">
+                    <p class="info-label">Likes</p>
+                    <span class="info-value">${t.likes}</span>
+                </div>
+                <div class="info-item">
+                    <p class="info-label">Views</p>
+                    <span class="info-value">${t.views}</span>
+                </div>
+                <div class="info-item">
+                    <p class="info-label">Comments</p>
+                    <span class="info-value">${t.comments}</span>
+                </div>
+                <div class="info-item">
+                    <p class="info-label">Downloads</p>
+                    <span class="info-value">${t.downloads}</span>
+                </div>
+            </div>
+        </a>
+    </li>
+    `}function p(t){return t.map(u).join("")}const i={formElem:document.querySelector(".form"),hitsListElem:document.querySelector(".gallery")};i.formElem.addEventListener("submit",async t=>{t.preventDefault();const a=new FormData(t.target).get("query"),o=await f(a,1);console.log(o);const e=p(o.hits);i.hitsListElem.innerHTML=e,t.target.reset()});
 //# sourceMappingURL=index.js.map
